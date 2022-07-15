0f56ad118b3934c339995b84098e414804118e45
Ronier Ramos <ronier2332@gmail.com>
2022-07-15 08:51:13 +0200

    adding map container to app entry point and styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MapContainer from "./pages/MapContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
